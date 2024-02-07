33484d625999fb974b3875dd6f8e70da6608c1cb
Basil <basil.jacob0007@gmail.com>
2024-02-07 16:39:28 +0530

    lazy load images

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,20 @@
 var $ = jQuery.noConflict();
     $(window).load(function() {
     $('.flexslider').flexslider({
-          animation: "fade"
+          animation: "fade",
+		  start: function (slider) {
+			// lazy load
+			$(slider).find("img.lazy").slice(0,5).each(function () {
+			var src = $(this).attr("data-src");
+			   $(this).attr("src", src).removeAttr("data-src").removeClass("lazy");
+			});
+		  },
+		 before: function (slider) {
+			 // lazy load
+			var slide = $(slider).find('.slides').children().eq(slider.animatingTo+1).find('img');
+			var src = slide.attr("data-src");
+			slide.attr("src", src).removeAttr("data-src").removeClass("lazy");
+		 }
     });
 	
 	$(function() {
